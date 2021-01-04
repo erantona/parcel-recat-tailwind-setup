0c58a1f9c38b3b82c322c8e5a4276aa4f21b43ee
erantona <roysuvendu1557.gmail.com>
2021-01-04 17:27:57 +0530

    ak47

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const App = () => {
+  return (
+    <div>
+      <h1 className="m-auto text-8xl font-bold">clean react with parcel</h1>
+    </div>
+  );
+};
+
+export default App;
