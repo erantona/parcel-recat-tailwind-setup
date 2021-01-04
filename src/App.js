8fef05588ae93b42b773b91efd3d9fa3462251f3
erantona <roysuvendu1557.gmail.com>
2021-01-04 17:32:39 +0530

    loki

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 const App = () => {
   return (
-    <div>
-      <h1 className="m-auto text-8xl font-bold">clean react with parcel</h1>
+    <div className="bg-pink-300 pt-20 w-screen h-screen">
+      <h1 className="m-auto text-8xl font-bold text-blue-700">
+        clean react with parcel & tailwind
+      </h1>
     </div>
   );
 };
